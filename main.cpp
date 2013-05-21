490b2743f0ff95aa12dd0971e5030d52f8423060
mitulrm <mitulmodi15@gmail.com>
2013-05-21 16:30:07 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QPalette>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("Calculator");
+    QPalette Pal;
+    Pal.setColor(QPalette::Background, Qt::black);
+    w.setAutoFillBackground(true);
+    w.setPalette(Pal);
+    w.setStyleSheet("QPushButton:hover { color:yellow }");
+
     w.show();
-    
     return a.exec();
 }
